505f80753ca4b03319c25c4a40e891c0e65cdd64
Darius Goore <dariusgoore@gmail.com>
2018-11-28 07:36:07 -0500

    set up models and controllers
    setting up models with sequelize and postgres,
    
    creating Authentication controller within server side and moving some routing logic to the controller.
    
    Everything working as expected through Part 2 of tutorial at 18:40

---
+++ server/src/config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  port: process.env.PORT || 8081,
+  db: {
+    database: process.env.DB_NAME || 'tabtutorial',
+    user: process.env.DB_USER || 'dariusgoore',
+    password: process.env.DB_PASS || 'tabtutorial',
+    options: {
+      dialect: process.env.DIALECT || 'postgres',
+      host: process.env.HOST || 'localhost'
+      // storage: './tabtutorial.postgres'
+    }
+  }
+}
