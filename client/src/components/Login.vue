775f41740aec142069833c0b3ddb6d6fc676e56f
Darius Goore <dariusgoore@gmail.com>
2018-11-30 17:20:12 -0500

    adding vuex for state management, essentially storing the current user_id and token up to date thru approx 58:00

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -3,19 +3,23 @@
     <v-flex xs6 offset-xs3>
       <div class="white elevation-2">
         <v-toolbar flat dense class="cyan" dark>
-          <v-toolbar-title>Log in</v-toolbar-title>
+        <v-toolbar-title>Log in</v-toolbar-title>
         </v-toolbar>
 
         <div class="pl-4 pr-4 pt-2 pb-2">
-          <v-text-field
-              label="Email"
-              v-model='email'
-            ></v-text-field>
-          <br>
-          <v-text-field
-              label="Password"
-              v-model='password'
-            ></v-text-field>
+           <form>
+             <v-text-field
+                label="Email"
+                v-model='email'
+              ></v-text-field>
+              <br>
+              <v-text-field
+                label="Password"
+                type="password"
+                v-model='password'
+              ></v-text-field>
+
+            </form>
           <br>
           <div class="error" v-html="error" />
           <v-btn dark class='cyan'
@@ -41,10 +45,12 @@ export default {
   methods: {
     async login () {
       try {
-        await AuthenticationService.login({
+        const response = await AuthenticationService.login({
           email: this.email,
           password: this.password
         })
+        this.$store.dispatch('setToken', response.data.token)
+        this.$store.dispatch('setUser', response.data.token)
       } catch (error) {
         this.error = error.response.data.error
       }
--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -14,6 +14,7 @@
           <br>
           <v-text-field
               label="Password"
+              type="password"
               v-model='password'
             ></v-text-field>
           <br>
@@ -41,10 +42,12 @@ export default {
   methods: {
     async register () {
       try {
-        await AuthenticationService.register({
+        const response = await AuthenticationService.register({
           email: this.email,
           password: this.password
         })
+        this.$store.dispatch('setToken', response.data.token)
+        this.$store.dispatch('setUser', response.data.token)
       } catch (error) {
         this.error = error.response.data.error
       }
