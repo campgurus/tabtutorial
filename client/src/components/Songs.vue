cd48706aa1bfc91a9f4ed7c0fb4d6f1a962b2f26
Darius Goore <dariusgoore@gmail.com>
2018-12-01 23:06:14 -0500

    created songs database
    created Songs controller with ndex and show actions.
    
    created service to connect to the songs endpoint on the server side.
    
    created Songs model to describe the table records
    
    Everything in order thru 37:36 of Part 4.

--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -2,23 +2,38 @@
   <v-layout row>
     <v-flex xs6 offset-xs3>
       <panel title='Songs and Stories' >
-        
+        <div
+          v-for="song in songs"
+          :key="song.id">
+          {{song.title}} -
+          {{song.artist}} -
+          {{song.album}}
+        </div>
       </panel>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
+import SongsService from '@/services/SongsService'
 import Panel from '@/components/Panel'
 export default {
   components: {
     Panel
+  },
+  data () {
+    return {
+      songs: null
+    }
+  },
+  async mounted () {
+    // do a request to the backend for all the Songs
+    this.songs = (await SongsService.index()).data
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 
 </style>
