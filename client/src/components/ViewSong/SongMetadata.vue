2627f423348907995c35002be4287ac47b952afd
Darius Goore <dariusgoore@gmail.com>
2018-12-03 23:15:34 -0500

    refactoring views to break down song/show into 4 subcomponents
    adding youtube embed

---
+++ client/src/components/ViewSong/SongMetadata.vue
@@ -0,0 +1,68 @@
+<template>
+  <panel title='Song Metadata'>
+    <v-layout>
+      <v-flex xs6>
+        <div class="song-title">
+          {{song.title}}
+        </div>
+        <div class="song-artist">
+          {{song.artist}}
+        </div>
+        <div class="song-genre">
+          {{song.genre}}
+        </div>
+      </v-flex>
+      <v-flex xs6>
+        <img class="album-image" :src="song.albumImageUrl" />
+        <br />
+        <div class="song-album">
+          {{song.album}}
+        </div>
+      </v-flex>
+    </v-layout>
+  </panel>
+</template>
+
+<script>
+import Panel from '@/components/Panel'
+export default {
+  components: {
+    Panel
+  },
+  props: [
+    'song'
+  ]
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.song {
+  padding: 20px;
+  height: 330px;
+  overflow: hidden;
+}
+.song-title {
+  font-size: 30px;
+}
+.song-artist {
+  font-size: 24px;
+}
+.song-genre {
+  font-size: 18px;
+}
+.album-image {
+  width: 70%;
+  margin: 0 auto;
+}
+textarea {
+  width: 100%;
+  font-family: monospace;
+  border: none;
+  height: 600px;
+  border-style: none;
+  border-color: transparent;
+  overflow: auto;
+  padding: 40px;
+}
+</style>
---
+++ client/src/components/ViewSong/Tab.vue
@@ -0,0 +1,25 @@
+<template>
+  <panel title="Tab">
+    <v-textarea
+      readonly
+      v-model="song.tab"
+    ></v-textarea>
+  </panel>
+</template>
+
+<script>
+import Panel from '@/components/Panel'
+export default {
+  props: [
+    'song'
+  ],
+  components: {
+    Panel
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
